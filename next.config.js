a977ee2ba88ef77c58c90273518ecc51d1aa00de
kouheisatou <koheisato0420@gmail.com>
2025-08-21 18:39:46 +0900

    deploy to github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/wecreate3_lp',
+  assetPrefix: '/wecreate3_lp',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     optimizePackageImports: ['react-icons'],
   },
